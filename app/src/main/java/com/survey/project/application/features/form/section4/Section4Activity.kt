a92e18cb7a17835eee20e4f611a8d4b4ebec3ba9
Elina Karanjit <elina.karanjit@ekbana.info>
2020-10-06 19:04:45 +0545

    Add Section 4 Activity

---
+++ app/src/main/java/com/survey/project/application/features/form/section4/Section4Activity.kt
@@ -0,0 +1,12 @@
+package com.survey.project.application.features.form.section4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.survey.project.application.R
+
+class Section4Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_section4)
+    }
+}
